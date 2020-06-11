ef090c1929470c69028f1ea8d39d1eab8eb84540
CRISS SOSA <ing.criscamih7gmail.com>
2020-06-11 11:33:41 -0500

    modulo calculadora go

---
+++ calculadora.go
@@ -0,0 +1,44 @@
+package myCalculator
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type mathStruct struct{}
+
+func (mathStruct) operate(entrada string, operador string) int {
+	numeros := strings.Split(entrada, operador)
+	num1 := parsear(numeros[0])
+	num2 := parsear(numeros[1])
+
+	switch operador {
+	case "+":
+		return num1 + num2
+	case "-":
+		return num1 - num2
+	case "*":
+		return num1 * num2
+	case "/":
+		return num1 / num2
+	default:
+		println(operador + " no está soportado")
+		return 0
+	}
+}
+
+func parsear(entrada string) int {
+	numero, _ := strconv.Atoi(entrada) //convierte de string a enterow
+	return numero
+}
+
+func leerEntrada() string {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	return scanner.Text()
+}
+
+//subir el modulo a git:(crear el repo primero en github)
+//go mod init github.com/usergit/reponame
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/criscamih/mycalculator
+
+go 1.14
